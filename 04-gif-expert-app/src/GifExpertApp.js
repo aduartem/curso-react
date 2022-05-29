cef1503cce16c1bebada904d48a39c53294e72b8
Andres Duarte M <andres.duarte.marquez@gmail.com>
2022-05-29 04:39:15 -0400

    Creando una lista de categorías y componente AddCategory

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 const GitExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+  // const handleAdd = () => {
+  //   setCategories([...categories, 'HunterXHunter']);
+  // };
+
   return (
     <>
       <h2>GitExpertApp</h2>
+
+      <AddCategory />
+
       <hr />
+      <ol>
+        {
+          categories.map(category => {
+            return (
+              <li key={category}>{category}</li>
+            )
+          })
+        }
+      </ol>
     </>
   );
 };
