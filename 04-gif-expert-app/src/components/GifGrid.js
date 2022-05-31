d9afa3a892f156164fa5df6de7ab198ffffb7a29
Andres Duarte M <andres.duarte.marquez@gmail.com>
2022-05-30 23:04:02 -0400

    useEffect

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export const GifGrid = ({category}) => {
+
+  useEffect(() => {
+    getGifs();
+  }, []);
 
   const getGifs = async () => {
     const url = 'https://api.giphy.com/v1/gifs/search?api_key=cxFEwYkkSqLvK6S2UB2hP2N7A0iTbS4X&q=Rick and Morty';
