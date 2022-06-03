89a467b564ff4e1e9117ee43d1cd58295a0afa6f
Andres Duarte M <andres.duarte.marquez@gmail.com>
2022-06-03 00:20:04 -0400

    Helpers - getGifs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,29 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const url = 'https://api.giphy.com/v1/gifs/search?api_key=cxFEwYkkSqLvK6S2UB2hP2N7A0iTbS4X&q=Rick and Morty';
-    const resp = await fetch(url);
-    const {data} = await resp.json();
-
-    const gifs = data.map(img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    });
-    setImages(gifs);
-  };
-
+  useEffect( () => {
+    getGifs(category)
+      .then(setImages);
+  }, [category]);
 
   return (
     <>
